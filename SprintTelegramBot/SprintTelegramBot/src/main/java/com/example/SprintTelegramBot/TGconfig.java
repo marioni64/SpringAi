e9b814f66e1d9071e970964efa55467c120a11d5
marioni64 <148502369+marioni64@users.noreply.github.com>
2024-06-26 16:54:15 +0400

    Add files via upload

---
+++ SprintTelegramBot/SprintTelegramBot/src/main/java/com/example/SprintTelegramBot/TGconfig.java
@@ -0,0 +1,30 @@
+package com.example.SprintTelegramBot;
+
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class TGconfig {
+
+    @SneakyThrows
+    @Bean
+    public TelegramBot telegramBot(@Value("${bot.token}") String botToken, TelegramBotsApi telegramBotsApi){
+
+        var botOptions = new DefaultBotOptions();
+        var bot = new TelegramBot(botOptions, botToken);
+        telegramBotsApi.registerBot(bot);
+        return bot;
+    }
+
+
+    @SneakyThrows
+    @Bean
+    public TelegramBotsApi telegramBotsApi(){
+        return new TelegramBotsApi(DefaultBotSession.class);
+    }
+}
